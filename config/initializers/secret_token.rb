c12f859a7d66527ff35859d66372a605021909dc
sanemat <o.gata.ken@gmail.com>
2012-10-08 20:39:42 +0900

    bundle exec rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinimalJson::Application.config.secret_token = '393c72aa131cdb9af8fbfa57673d49dc770c716dfec973cf3804e1a66d949f64a388f96a352f9e8d0eaa2df29057a02ed7303994b44652df32589de2ddbacb62'
